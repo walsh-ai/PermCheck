40589683393c44bfd4702a8433ee981cd47990d9
csnirs <walsh.jthomas@outlook.com>
2023-09-05 16:32:07 +0100

    First commit, my solution and simple test.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic; 
+
+// Top level statements
+// Test the solution here 
+int[] basis = {1, 2, 3, 4, 5}; 
+int[] perm = {5, 4, 3, 1, 2};
+int[] excp = {2, 3, 4, 5, 6}; 
+
+Console.WriteLine("Basis array: 1, 2, 3, 4, 5"); 
+Console.WriteLine($"Array 5, 4, 3, 2, 1 is a permutation: {IsPermutation(basis, perm)}");
+Console.WriteLine($"Array 2, 3, 4, 5, 6 is a permutation: {IsPermutation(basis, excp)}");
+
+public static partial class Program {
+    public static bool IsPermutation(int[] a, int[] b) {
+        Dictionary<int, bool> elementSet = new Dictionary<int, bool>();
+
+        // Each element is keyed with value zero 
+        foreach (int i in a) {
+            if (!elementSet.ContainsKey(i))
+                elementSet.Add(i, false); 
+        }
+        
+        // For each element in b, check if it is in set 
+        foreach (int i in b) {
+            if (elementSet.ContainsKey(i)) {
+                if (elementSet[i])
+                    return false; 
+                else 
+                    elementSet[i] = true; 
+            } else 
+                return false; 
+        }
+
+        return true; 
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Receive a non-empty array A consisting of N integers. 
+
+Then, a permutation of this array is a sequence containing all the 
+array elements (indexed 0 ... N-1) only once. 
+
+Given a second array, check if it is a permutation of A. 
+
